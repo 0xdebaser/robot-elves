b3ec4b29ed564e2e2b04a86cd433759b86315e74
0xdebaser <0xdebaser@gmail.com>
2022-11-30 14:05:51 -1000

    title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
+import Head from "next/head";
+
 import "./styles.css";
 import "../components/input/inputPrompts.styles.scss";
 import "../components/buttons/inputButton.styles.scss";
 import "../components/letter/letter.styles.scss";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>robot santa | 🤖🎅</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 export default App;
